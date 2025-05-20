fa5ec1ba583e6fddb3ba1e9b1281768db99f2917
Lukas Potthast <privat@lukas-potthast.de>
2025-05-20 19:39:48 +0200

    Format code

--- src/error.rs
+++ src/error.rs
@@ -8,7 +8,11 @@ pub struct InvalidS3PathComponent {
 
 impl std::fmt::Display for InvalidS3PathComponent {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Invalid S3 path component '{}': {}", self.component, self.reason)
+        write!(
+            f,
+            "Invalid S3 path component '{}': {}",
+            self.component, self.reason
+        )
     }
 }
 
